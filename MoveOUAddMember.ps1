4e167f00d87999a801f7e5edff8c279760f7b1b5
Dee <99658722+DeeJ4yNg@users.noreply.github.com>
2022-06-03 15:28:52 +0800

    Update MoveOUAddMember.ps1

--- MoveOUAddMember.ps1
+++ MoveOUAddMember.ps1
@@ -1,11 +1,6 @@
 
-$GLOBAL:PackageName = "batch_move_ou"
-$GLOBAL:criptRoot = Split-Path -Path $MyInvocation.MyCommand.Definition -parent
-$global:LastExitCode = ""
-."$criptRoot\support\spprtfnctns_.ps1"
-
 import-module ActiveDirectory
-$MoveList = Import-Csv -Path "$criptRoot\moveou_list.csv" 
+$MoveList = Import-Csv -Path "list.csv" 
 
 $countPC = 0
 foreach($data in $MoveList){
@@ -14,11 +9,10 @@ foreach($data in $MoveList){
     $TargetOU = "OU=xx, OU=xx,OU=xx,DC=xx,DC=xx,DC=xx,DC=xx"
     Try{ 
         Get-ADComputer $com | Move-ADObject -TargetPath $TargetOU
-        IW-LogEntry "$com moved to $TargetOU"
-           $countPC++
+        $countPC++
     }catch{
-        IW-LogEntry "Error - Cannot move $com"
+        write-host "Error - Cannot move $com"
    }
 }
 
-IW-LogEntry " $countPC  Computers has been moved "
+write-host " $countPC  Computers has been moved "
